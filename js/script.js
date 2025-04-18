f84b349e1352ef01faf814d90c4df858fec474bc
meetbhavanandji <meetbhavanandi2004@gmail.com>
2025-04-18 14:59:03 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let gameNum = 8181;
+
+let userNum = prompt("enter a number:");
+
+while (userNum != gameNum) {
+  userNum = prompt("you enter wrong number, try again ");
+}
+console.log("congretutions");
